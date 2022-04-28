4311f94eb370d87f7af5b983ce6c8e59184a034c
1951603-Chen Jiahui <174856792@qq.com>
2022-04-28 18:54:22 +0800

    finish lab lazy

--- kernel/syscall.c
+++ kernel/syscall.c
@@ -68,7 +68,20 @@ int
 argaddr(int n, uint64 *ip)
 {
   *ip = argraw(n);
-  return 0;
+	if (walkaddr(myproc()->pagetable, *ip) == 0) {
+		if (*ip >= myproc()->sz || *ip <= myproc()->trapframe->sp)
+			return -1;
+		uint64 va = PGROUNDDOWN(*ip);
+		uint64 pa = (uint64)kalloc(); 
+		if (pa == 0)
+			return -1;
+		memset((void *)pa, 0, PGSIZE);
+		if (mappages(myproc()->pagetable, va, PGSIZE, pa, PTE_W | PTE_R | PTE_U) != 0) {
+			kfree((void *)pa);
+			return -1;
+		}
+	}
+	return 0;
 }
 
 // Fetch the nth word-sized system call argument as a null-terminated string.
