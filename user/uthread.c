3ffdf482207178cbd2a7fa96edcc2e8871cc85bd
Chen Jiahui <1748567982@qq.com>
2022-05-10 23:13:20 +0800

    finish lab7

--- user/uthread.c
+++ user/uthread.c
@@ -9,12 +9,32 @@
 
 #define STACK_SIZE  8192
 #define MAX_THREAD  4
+#define PGSIZE 4096
+
+struct context {
+  uint64 ra;
+  uint64 sp;
+
+  // callee-saved
+  uint64 s0;
+  uint64 s1;
+  uint64 s2;
+  uint64 s3;
+  uint64 s4;
+  uint64 s5;
+  uint64 s6;
+  uint64 s7;
+  uint64 s8;
+  uint64 s9;
+  uint64 s10;
+  uint64 s11;
+};
 
 
 struct thread {
   char       stack[STACK_SIZE]; /* the thread's stack */
   int        state;             /* FREE, RUNNING, RUNNABLE */
-
+	struct context context;				/* save regs */
 };
 struct thread all_thread[MAX_THREAD];
 struct thread *current_thread;
@@ -63,6 +83,7 @@ thread_schedule(void)
      * Invoke thread_switch to switch from t to next_thread:
      * thread_switch(??, ??);
      */
+		thread_switch((uint64)&t->context, (uint64)&next_thread->context);
   } else
     next_thread = 0;
 }
@@ -77,6 +98,8 @@ thread_create(void (*func)())
   }
   t->state = RUNNABLE;
   // YOUR CODE HERE
+	t->context.ra = (uint64)func;
+	t->context.sp = (uint64)t->stack + PGSIZE;	
 }
 
 void 
@@ -89,6 +112,15 @@ thread_yield(void)
 volatile int a_started, b_started, c_started;
 volatile int a_n, b_n, c_n;
 
+/*
+void info() 
+{
+	for (int i = 0; i < 4; i ++)
+		printf("%d ", all_thread[i].state);
+	printf("\n");
+}
+*/
+
 void 
 thread_a(void)
 {
@@ -100,6 +132,7 @@ thread_a(void)
   
   for (i = 0; i < 100; i++) {
     printf("thread_a %d\n", i);
+		// info();
     a_n += 1;
     thread_yield();
   }
@@ -120,6 +153,7 @@ thread_b(void)
   
   for (i = 0; i < 100; i++) {
     printf("thread_b %d\n", i);
+		// info();
     b_n += 1;
     thread_yield();
   }
@@ -140,6 +174,7 @@ thread_c(void)
   
   for (i = 0; i < 100; i++) {
     printf("thread_c %d\n", i);
+		// info();
     c_n += 1;
     thread_yield();
   }
